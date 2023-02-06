95c2881fbef04ce84e03d06c7c9ce157392edb33
ethanvannest <ethan3vannest@gmail.com>
2023-02-06 13:48:10 -0700

    created score component and an answer system

--- python_studying.py
+++ python_studying.py
@@ -1,36 +1,44 @@
+# IMPORTS
 import random
 
 print('hello world')
+
+score = 0
+
+# DICTIONARY FOR QUESTIONS FOR STUDY GUIDE
 questions = {
     '1': [
-        'What is a default and conversion constructor?',
-        'What are the advantages and disadvantages of multiple inheritance?',
-        'What is a singleton class?',
-        'What is a stream?',
-        'What is a view?',
-        'What is an object?',
-        'What is polymorphism?',
-        'Is string class final?',
-        'What is the difference between overriding and overloading?',
-        'What is a character stream?',
-        'What is a file?',
-        'What is a class? What is a superclass?',
-        'What is inheritance?',
-        'What are the primary components of a computer system?',
-        'What is a chipset?',
-        'What is an operating system? What are the popular operating systems used today?',
-        'What is primary and secondary memory?',
-        'What are the commonly used computer processors?',
-        'What is a constructor?',
-        'What is an interface?',
+        'What is a default and conversion constructor? ',
+        'What are the advantages and disadvantages of multiple inheritance? ',
+        'What is a singleton class? ',
+        'What is a stream? ',
+        'What is a view? ',
+        'What is an object? ',
+        'What is polymorphism? ',
+        'Is string class final? ',
+        'What is the difference between overriding and overloading? ',
+        'What is a character stream? ',
+        'What is a file? ',
+        'What is a class? What is a superclass? ',
+        'What is inheritance? ',
+        'What are the primary components of a computer system? ',
+        'What is a chipset? ',
+        'What is an operating system? What are the popular operating systems used today? ',
+        'What is primary and secondary memory? ',
+        'What are the commonly used computer processors? ',
+        'What is a constructor? ',
+        'What is an interface? ',
         ]
 }
 answer = input('what are you wanting to study? 1.Computer Science Principles 2. Syntax 3. Make your own study guide. ')
 
-print(random.randint(0,9))
+num = random.randint(0,19)
 
 if answer == '1':
-    print('=====Computer Science Principles======')
-    num = random.randint(0,19)
-    print(questions['1'][num])
+    print('===== Computer Science Principles ======')
+    comp_answer = input(questions['1'][num])
+    next_num = num + 1
+    if comp_answer == questions['1'][next_num]:
+        score++ 
+        print('Correct', 'Score:',score)
 # print(answer)
